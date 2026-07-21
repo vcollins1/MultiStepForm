6956fbfbc20de9a63185e0299595bf6bddccb25c
Vontrelle Collins <vontrellecollins@gmail.com>
2026-07-21 17:52:31 -0400

    save checked add-ons and css updates for Blazor CSS isolation.

--- Services/StepsData.cs
+++ Services/StepsData.cs
@@ -10,10 +10,10 @@ public class StepsData
         (3, "Pro", 15, "/images/icon-pro.svg")
     ];
 
-    public static readonly (string Title, string Description, decimal Price)[] AddOns =
+    public static readonly (int Id, string Title, string Description, decimal Price)[] AddOns =
     [
-        ("Online service", "Access to multiplayer games", 1),
-        ("Larger storage", "Extra 1TB of cloud save", 2),
-        ("Customizable profile", "Custom theme on your profile", 2)
+        (0, "Online service", "Access to multiplayer games", 1),
+        (1, "Larger storage", "Extra 1TB of cloud save", 2),
+        (2, "Customizable profile", "Custom theme on your profile", 2)
     ];
 }
